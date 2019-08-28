31a6b4abf65833d8a48f88b6a9635e76895455ad
why1115 <34000872+why1115@users.noreply.github.com>
2019-08-28 16:24:59 +0800

    Add files via upload

---
+++ cform_config.php
@@ -0,0 +1,9 @@
+<?php
+ error_reporting(0);
+  
+  // configuration
+  $page_title = "Contact Us Form";
+  $email_it_to = "YourEmailHere@YourSite.com"; //change to your email here
+  $error_message = "Please complete the form first";
+  $confirmation = "Thank you, your message has been successfully sent.";
+?>
